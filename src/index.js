ed19c8d0ec3130b21d64c7671da58f8faef18af4
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-06-09 07:48:43 +0530

    Utils Code Discussion

--- src/index.js
+++ src/index.js
@@ -10,16 +10,20 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is Running at port : ${process.env.PORT}`);
+    });
 
-
-
-
-
-
-
-
-
+    // app.on("error", (error) => {
+    //   console.log("Error : ", error);
+    //   throw error;
+    // });
+  })
+  .catch((err) => {
+    console.log("MONGOOSE CONNECTION FIALED !!!", err);
+  });
 
 /*
 import express from "express";
